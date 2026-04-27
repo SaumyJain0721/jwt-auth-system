9729c1cbe54daab8131687066afa47d44d5fddec
Saumy Jain <saumy0721@gmail.com>
2026-04-28 02:54:28 +0530

    JWT authentication fully integrated with protected endpoints

--- src/main/java/com/saumyproject/jwt_auth_system/dto/LoginResponse.java
+++ src/main/java/com/saumyproject/jwt_auth_system/dto/LoginResponse.java
@@ -1,25 +1,13 @@
 package com.saumyproject.jwt_auth_system.dto;
 
 public class LoginResponse {
-
-    private String message;
     private String token;
-    private Long id;
-    private String name;
-
-    public LoginResponse(String message, String token, Long id, String name) {
-        this.message = message;
-        this.token = token;
-        this.id = id;
-        this.name = name;
-    }
 
     public LoginResponse(String token) {
         this.token = token;
     }
 
-    public String getMessage() { return message; }
-    public String getToken() { return token; }
-    public Long getId() { return id; }
-    public String getName() { return name; }
+    public String getToken() {
+        return token;
+    }
 }
---
+++ src/main/java/com/saumyproject/jwt_auth_system/security/JwtAuthFilter.java
@@ -0,0 +1,44 @@
+package com.saumyproject.jwt_auth_system.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            jwtService.extractEmail(token);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
